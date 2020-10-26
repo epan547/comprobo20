bd6228549dfe4340d31fdbbb2bb0c20c018edf7d
epan547 <epan547@gmail.com>
2020-10-26 13:26:55 -0400

    neato soccer

--- neato_soccer/scripts/ball_tracker.py
+++ neato_soccer/scripts/ball_tracker.py
@@ -26,11 +26,43 @@ class BallTracker(object):
         rospy.Subscriber(image_topic, Image, self.process_image)
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         cv2.namedWindow('video_window')
+        cv2.namedWindow("binary_image", cv2.WINDOW_AUTOSIZE)
+        # cv2.setMouseCallback('video_window', self.process_mouse_event)
+        self.angular_k = -1
+        self.angular_vel = 0
+        self.linear_vel = .3
+        self.center_x = 300
+
+    def process_mouse_event(self, event, x,y,flags,param):
+        """ Process mouse events so that you can see the color values
+            associated with a particular pixel in the camera images """
+        image_info_window = 255*np.ones((500,500,3))
+        cv2.putText(image_info_window,
+                    'Color (b=%d,g=%d,r=%d)' % (self.cv_image[y,x,0], self.cv_image[y,x,1], self.cv_image[y,x,2]),
+                    (5,50),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0,0,0))
+
+        cv2.imshow('image_info', image_info_window)
+        cv2.waitKey(5)
 
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
         self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+        self.binary_image = cv2.inRange(self.cv_image, (0, 0, 100), (20,20, 255))
+        moments = cv2.moments(self.binary_image)
+        if moments['m00'] != 0:
+            self.center_x, self.center_y = moments['m10']/moments['m00'], moments['m01']/moments['m00']
+
+            # Calculate error between current and desired position
+            normalized_x = (self.center_x - 300 ) / 600
+
+            # Calculate velocities using proportional control
+            self.angular_vel = self.angular_k * normalized_x
+            self.linear_vel = .3
+
 
     def run(self):
         """ The main run loop, in this node it doesn't do anything """
@@ -38,9 +70,10 @@ class BallTracker(object):
         while not rospy.is_shutdown():
             if not self.cv_image is None:
                 print(self.cv_image.shape)
+                cv2.imshow('binary_image', self.binary_image)
                 cv2.imshow('video_window', self.cv_image)
                 cv2.waitKey(5)
-
+                self.pub.publish(Twist(linear=Vector3(x=self.linear_vel), angular=Vector3(z=self.angular_vel)))
             # start out not issuing any motor commands
             r.sleep()
 
